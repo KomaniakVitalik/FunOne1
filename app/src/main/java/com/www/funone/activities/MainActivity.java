8168091889e85a8194b73d40a002f2723e66c5f5
vitaliy.komaniak <vitaliy.komaniak@vakoms.com.ua>
2016-06-25 17:33:19 +0300

    ADD: settings activity

--- app/src/main/java/com/www/funone/activities/MainActivity.java
+++ app/src/main/java/com/www/funone/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.www.funone.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
