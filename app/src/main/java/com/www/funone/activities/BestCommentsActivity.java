ac6719ad423ccb87193138f57816751f2d8d8807
vitaliy.herasymchuk <vitaliy.herasymchuk@vakoms.com>
2016-06-27 17:46:30 +0300

    removed viewpager from BestCommentsActivity.java instead used simple layouts

--- app/src/main/java/com/www/funone/activities/BestCommentsActivity.java
+++ app/src/main/java/com/www/funone/activities/BestCommentsActivity.java
@@ -1,41 +1,29 @@
 package com.www.funone.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.www.funone.R;
 import com.www.funone.fragments.CommentsFragment;
-import com.www.funone.view.NonSwipableViewPager;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class BestCommentsActivity extends BaseActivity {
+public class BestCommentsActivity extends BaseActivity implements View.OnClickListener {
 
     public static final String ARG_POST = "post";
-
-    private TabLayout tabLayout;
-    private NonSwipableViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_best_comments);
         setUpToolBar();
-        setupView();
         removeIncludedLayoutMarginBottom();
         resizeIncludedLayoutImageView();
+        loadFrg(CommentsFragment.newInstance(), R.id.content_best_comments);
+        findViewById(R.id.all_comments).setOnClickListener(this);
+        findViewById(R.id.best_comments).setOnClickListener(this);
     }
 
     private void resizeIncludedLayoutImageView() {
@@ -69,76 +57,36 @@ public class BestCommentsActivity extends BaseActivity {
         });
     }
 
-    private void setupViewPager() {
-        viewPager = (NonSwipableViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-        viewPager.setPagingEnabled(false);
-
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-        viewPager.setCurrentItem(0);
-
-        tabLayout.setSelectedTabIndicatorHeight(0);
-
-        tabLayout.getTabAt(0).setCustomView(bestCommentsView());
-        tabLayout.getTabAt(1).setCustomView(allCommentsView());
-
-        tabLayout.getTabAt(0).getCustomView().setSelected(true);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
-
-    private View bestCommentsView() {
-        return LayoutInflater.from(this).inflate(R.layout.tab_best_comments, null);
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
-    private View allCommentsView() {
-        return LayoutInflater.from(this).inflate(R.layout.tab_all_comments, null);
-    }
-
-
-    private void setupViewPager(ViewPager viewPager) {
-        CommentsPageAdapter adapter = new CommentsPageAdapter(getSupportFragmentManager());
-        adapter.addFragment(CommentsFragment.newInstance(), null);
-        adapter.addFragment(CommentsFragment.newInstance(), null);
-        viewPager.setAdapter(adapter);
-    }
-
-    private void setupView() {
-        setupViewPager();
-    }
-
-    public class CommentsPageAdapter extends FragmentPagerAdapter {
-
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public CommentsPageAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        findViewById(R.id.best_comments).setSelected(true);
+        findViewById(R.id.all_comments).setSelected(false);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.all_comments:
+                findViewById(R.id.best_comments).setSelected(false);
+                findViewById(R.id.all_comments).setSelected(true);
+                startActivity(new Intent(this, AllCommentActivity.class));
+                break;
+        }
     }
 }
