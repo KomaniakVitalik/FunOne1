000ec8977d6618ef02a7a53c84a93af2eaf661e7
vitaliy.komaniak <vitaliy.komaniak@vakoms.com.ua>
2016-06-27 21:08:37 +0300

    ADD: settings

--- app/src/main/java/com/www/funone/CoreApp.java
+++ app/src/main/java/com/www/funone/CoreApp.java
@@ -3,6 +3,9 @@ package com.www.funone;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
+import com.google.gson.Gson;
+import com.www.funone.util.AppSettings;
+import com.www.funone.util.Pref;
 
 /**
  * Created by vitaliy.herasymchuk on 6/25/16.
@@ -10,15 +13,30 @@ import com.facebook.FacebookSdk;
 public class CoreApp extends Application {
 
     private static CoreApp instance;
+    private AppSettings appSettings;
 
     @Override
     public void onCreate() {
         super.onCreate();
         FacebookSdk.sdkInitialize(this);
         instance = this;
+
+        if(Pref.getString(Pref.APP_SETTINGS_KEY) == null) {
+            appSettings = new AppSettings();
+        }else {
+            appSettings = new Gson().fromJson(Pref.getString(Pref.APP_SETTINGS_KEY), AppSettings.class);
+        }
     }
 
     public static CoreApp getInstance() {
         return instance;
+    }
+
+    public AppSettings getAppSettings() {
+        return appSettings;
+    }
+
+    public void setAppSettings(AppSettings appSettings) {
+        this.appSettings = appSettings;
     }
 }
