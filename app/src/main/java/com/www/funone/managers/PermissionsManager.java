b686df8b60149631a3bdd6b377eabcbabaaeaa97
vitaliy.herasymchuk <vitaliy.herasymchuk@vakoms.com>
2016-07-01 15:19:54 +0300

    ADD; comments to methods of PermissionsManager.java

--- app/src/main/java/com/www/funone/managers/PermissionsManager.java
+++ app/src/main/java/com/www/funone/managers/PermissionsManager.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
+
 import com.www.funone.CoreApp;
 import com.www.funone.util.Validator;
 
@@ -24,7 +25,17 @@ public class PermissionsManager {
         return instance;
     }
 
-    public static boolean hasPermissions(Activity activity, int requestCode, String... permissions) {
+    /**
+     * Checks whether app has an array of provided permissions enabled
+     *
+     * @param activity    - Activity
+     * @param requestCode - requestCode
+     * @param permissions - String[] permissions
+     * @return - true if all the permissions are granted
+     */
+    public static boolean hasPermissions(Activity activity,
+                                         int requestCode,
+                                         String... permissions) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && CoreApp.getInstance() != null && permissions != null) {
             for (String permission : permissions) {
                 if (ActivityCompat.checkSelfPermission(CoreApp.getInstance(), permission) != PackageManager.PERMISSION_GRANTED) {
@@ -41,7 +52,11 @@ public class PermissionsManager {
      * @param permissions  - Permissions array
      * @param grantResults - grant results array
      */
-    public static void handleDenyAndNeverAskAgain(Activity activity, String[] permissions, int[] grantResults, int requestCode, OnNeverAskAgainListener listener) {
+    public static void handleDenyAndNeverAskAgain(Activity activity,
+                                                  String[] permissions,
+                                                  int[] grantResults,
+                                                  int requestCode,
+                                                  OnNeverAskAgainListener listener) {
         if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
             if (shouldShowPermissionRationale(activity, permissions[0])) {
                 requestPermission(activity, permissions[0], requestCode);
@@ -58,20 +73,33 @@ public class PermissionsManager {
      * @param permission  - String permission value
      * @param requestCode - user's request code
      */
-    public static void requestPermission(Activity activity, String permission, int requestCode) {
+    public static void requestPermission(Activity activity,
+                                         String permission,
+                                         int requestCode) {
         ActivityCompat.requestPermissions(activity,
                 new String[]{permission},
                 requestCode);
     }
 
 
-    public static boolean shouldShowPermissionRationale(Activity activity, String permission) {
+    /**
+     * Determines if user didn't activate flag Never Ask Again. If no - shows
+     * permissions dialog again.
+     *
+     * @param activity   - Activity
+     * @param permission - Current Permission
+     * @return - true if never ask again was not activated
+     */
+    public static boolean shouldShowPermissionRationale(Activity activity,
+                                                        String permission) {
         return ActivityCompat.shouldShowRequestPermissionRationale(activity, permission);
     }
 
-    public interface OnNeverAskAgainListener{
+    /**
+     * Listener to call back when never ask again was pressed before
+     */
+    public interface OnNeverAskAgainListener {
         void onNeverAskAgain();
     }
-
 
 }
